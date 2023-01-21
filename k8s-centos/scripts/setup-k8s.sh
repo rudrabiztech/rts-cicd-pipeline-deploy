a813cb865079b33fd4ce5499a2025ed575a24625
makwaji <makwana.jignesh@gmail.com>
2023-01-21 17:40:20 +0800

    Added code to install docker

--- k8s-centos/scripts/setup-k8s.sh
+++ k8s-centos/scripts/setup-k8s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Running: ansible-playbook playbooks/k8s-all.yaml $*'
-ansible-playbook -u rts -i inventory/k8scluster playbooks/k8s-all.yaml
+ansible-playbook -u rts -i inventory/k8scluster playbooks/k8s-all.yaml $*
