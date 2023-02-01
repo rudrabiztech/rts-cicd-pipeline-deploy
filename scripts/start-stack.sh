57e330f03daa3151438bccca63d25be9a8056820
makwaji <makwana.jignesh@gmail.com>
2023-02-01 15:34:53 +0800

    Added code for nginxproxy manager

--- scripts/start-stack.sh
+++ scripts/start-stack.sh
@@ -3,3 +3,6 @@ git pull origin main
 
 # start docker container in backgound
 docker-compose --env-file ./config/.env.dev up -d --remove-orphans
+
+# start docker container nginx proxy manager in backgound
+docker-compose -f ./docker-compose-nginx.yaml up -d
--- scripts/stop-stack.sh
+++ scripts/stop-stack.sh
@@ -1,2 +1,5 @@
 #stop docker container
 docker-compose --env-file ./config/.env.dev stop
+
+#stop docker container nginx proxy manager
+docker-compose -f ./docker-compose-nginx.yaml stop
