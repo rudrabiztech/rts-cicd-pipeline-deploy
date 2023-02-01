8bc2cc2113d582afbf6209a01d90dd70c834d300
makwaji <makwana.jignesh@gmail.com>
2023-02-01 17:27:14 +0800

    Script updaetd

--- scripts/destroy-stack.sh
+++ scripts/destroy-stack.sh
@@ -1,5 +1,5 @@
 #destroy docker container
-docker-compose --env-file ./config/.env.dev down -v
+docker-compose -f ./docker-compose-sonar.yaml --env-file ./config/.env.dev down -v
 
 #destroy docker container
 docker-compose -f ./docker-compose-nginx.yaml --env-file ./config/.env.dev down -v
--- scripts/stop-stack.sh
+++ scripts/stop-stack.sh
@@ -1,5 +1,5 @@
-#stop docker container
-docker-compose --env-file ./config/.env.dev stop
+#stop docker sonar container
+docker-compose -f ./docker-compose-sonar.yaml --env-file ./config/.env.dev stop
 
 #stop docker container nginx proxy manager
 docker-compose -f ./docker-compose-nginx.yaml --env-file ./config/.env.dev stop
