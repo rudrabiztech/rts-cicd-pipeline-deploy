1da24a2ec2217228fdf739fc0bf864e97c53831c
devang704 <devangsybca00@gmail.com>
2023-02-06 22:57:32 +0530

    Added srvice of external redis-db for gitlab

--- scripts/destroy-stack.sh
+++ scripts/destroy-stack.sh
@@ -1,2 +1,2 @@
 #destroy docker container
-docker-compose down -v
+docker-compose --env-file ./config/.env.dev down -v
--- scripts/start-stack.sh
+++ scripts/start-stack.sh
@@ -1,5 +1,5 @@
 # Pull latest code
-git pull origin main
+#git pull origin main
 
 # start docker container in backgound
 docker-compose --env-file ./config/.env.dev up -d --remove-orphans
