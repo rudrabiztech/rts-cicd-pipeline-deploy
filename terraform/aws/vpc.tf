21c04b5fac4910306f14657f87fa6fcdef2589ed
makwaji <makwana.jignesh@gmail.com>
2023-02-21 20:41:47 +0800

    Added Public and private subnet

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -19,3 +19,102 @@ resource "aws_subnet" "aws-mysg-1a-web" {
     Name = "aws-mysg-1a-web"
   }
 }
+
+resource "aws_subnet" "aws-mysg-1a-app" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.aws-mysg-1a-app
+  availability_zone = var.sg-az1
+  tags = {
+    Name = "aws-mysg-1a-app"
+  }
+}
+
+resource "aws_subnet" "aws-mysg-1a-db" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.aws-mysg-1a-db
+  availability_zone = var.sg-az1
+  tags = {
+    Name = "aws-mysg-1a-db"
+  }
+}
+
+resource "aws_subnet" "aws-mysg-1b-web" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.aws-mysg-1b-web
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "aws-mysg-1b-web"
+  }
+}
+
+resource "aws_subnet" "aws-mysg-1b-app" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.aws-mysg-1b-app
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "aws-mysg-1b-app"
+  }
+}
+
+resource "aws_subnet" "aws-mysg-1b-db" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.aws-mysg-1b-db
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "aws-mysg-1b-db"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1a-web" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1a-web
+  availability_zone = var.sg-az1
+  tags = {
+    Name = "irn-mysg-1a-web"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1a-app" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1a-app
+  availability_zone = var.sg-az1
+  tags = {
+    Name = "irn-mysg-1a-app"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1a-db" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1a-db
+  availability_zone = var.sg-az1
+  tags = {
+    Name = "irn-mysg-1a-db"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1b-web" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1b-web
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "irn-mysg-1b-web"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1b-app" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1b-app
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "irn-mysg-1b-app"
+  }
+}
+
+resource "aws_subnet" "irn-mysg-1b-db" {
+  vpc_id            = aws_vpc.jig-aws-adn.id
+  cidr_block        = var.irn-mysg-1b-db
+  availability_zone = var.sg-az2
+  tags = {
+    Name = "irn-mysg-1b-db"
+  }
+}
